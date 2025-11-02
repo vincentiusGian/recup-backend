c4e740bbd9bebdc6a63ab9b4335d64ca9fe8e98c
vincentiusgian <gianjunius54@gmail.com>
2025-11-02 16:01:23 +0700

    buat register jadi responsive

--- app.py
+++ app.py
@@ -428,4 +428,4 @@ def payment_notification():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=False, host='0.0.0.0', port=5001)
